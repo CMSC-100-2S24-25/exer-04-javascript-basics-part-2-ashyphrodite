16f5eb9cb7b9135a46d3be284ea6d544b34f2bf4
ashyphrodite <rddimaano2@up.edu.ph>
2025-02-26 11:33:56 +0800

    test: ADD test cases for addAccount()

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 import { generateUniqueID } from './index.js';
+import { addAccount } from './index.js';
 
 console.log(generateUniqueID("Alan", "Turing"));        // aturing<uuid>
 console.log(generateUniqueID("Tim", "Berners-Lee"));    // tbernerslee<uuid>
 console.log(generateUniqueID("Grace", "Hopper"));       // ghopper<uuid>
 
+addAccount(["Alan", "Turing", "aturing@w3c.com", 25]);
+addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 30]);
+addAccount(["Grace", "Hopper", "ghopper@def.gov", 40]);
