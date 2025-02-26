fd180a65ce4d5f10e71ebf6fc285ea05c94d100a
ashyphrodite <rddimaano2@up.edu.ph>
2025-02-26 11:28:49 +0800

    test: ADD more test cases to generateUniqueID

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 import { generateUniqueID } from './index.js';
 
-console.log(generateUniqueID("Alan", "Turing"));
+console.log(generateUniqueID("Alan", "Turing"));        // aturing<uuid>
+console.log(generateUniqueID("Tim", "Berners-Lee"));    // tbernerslee<uuid>
+console.log(generateUniqueID("Grace", "Hopper"));       // ghopper<uuid>
+
