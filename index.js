5159252d07e288a7bc2a0c48cf6a979d5ea7b50f
ashyphrodite <rddimaano2@up.edu.ph>
2025-02-26 10:33:22 +0800

    chore: VALIDATE input parameters

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ import fs from 'fs';
 import validator from 'validator';
 
 function generateUniqueID(string1, string2) {
-    
+    // valdate input paramaters
+    if (!validator.isString(string1) || !validator.isString(string2)) {
+        return null;
+    }
 }
